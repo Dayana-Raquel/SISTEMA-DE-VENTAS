5e70e325128dcd083405179b2d36d7fba34d5fd3
Dayana-Raquel <dayana.bazualdo@sistemas.edu.bo>
2025-05-06 23:43:31 -0400

    Se realiza la conexión a la base de datos

---
+++ SistemaDeVentas/src/conexion/Conexion.java
@@ -0,0 +1,22 @@
+package conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author HP RYZEN 3
+ */
+public class Conexion {
+
+    public static Connection conectar() {
+        try {
+            Connection cn = DriverManager.getConnection("jbdc:mysql://localhost/bd_sistema_ventas", "root", "");
+            return cn;
+        } catch (SQLException e) {
+            System.out.println("Error en la conexion local " + e);
+        }
+        return null;
+    }
+}
